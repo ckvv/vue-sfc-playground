4163a169fa7b05b42a611010f23b2b0f3cc7bbe0
陈锴 <k_0520@qq.com>
2024-08-02 14:21:46 +0800

    chore: 更新注释

--- plugins/source-code.ts
+++ plugins/source-code.ts
@@ -3,6 +3,7 @@ import MagicString from 'magic-string';
 
 /**
  * 在指定位置插入代码
+ * 目前是通过在对应 SFC 指定位置插入代码 失信，针对复杂的情况可以通过 @vue/compiler-dom 将对应 SFC 文件 转为 AST 实现
  */
 export async function updateSourceCode(options: { file: string, code: string, endOffset: string }) {
   const beforeCode = (await readFile(options.file)).toString();
